a3e00f4e192e195907467bf3b3a740baf48082bb
Nana Yu <nana@kwusen.com>
2025-06-29 19:06:16 -0700

    Sync pdf annotations

--- src/display/editor/annotation_editor_layer.js
+++ src/display/editor/annotation_editor_layer.js
@@ -401,7 +401,9 @@ class AnnotationEditorLayer {
     }
 
     this.#cleanup();
-    this.div.hidden = this.isEmpty;
+    if (this.isEmpty) {
+      this.div.hidden = true;
+    }
     const { classList } = this.div;
     for (const editorType of AnnotationEditorLayer.#editorTypes.values()) {
       classList.remove(`${editorType._type}Editing`);
@@ -647,6 +649,7 @@ class AnnotationEditorLayer {
     } else {
       this.add(editor);
     }
+    this.div.hidden = this.isEmpty;
   }
 
   /**
--- src/display/editor/tools.js
+++ src/display/editor/tools.js
@@ -2386,6 +2386,14 @@ class AnnotationEditorUIManager {
   }
 
   /**
+   * Get all the editors.
+   * @returns {Array<AnnotationEditor>}
+   */
+  getAllEditors() {
+    return this.#allEditors.values();
+  }
+
+  /**
    * Get an editor with the given id.
    * @param {string} id
    * @returns {AnnotationEditor}
