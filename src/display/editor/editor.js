cb85e6d6f96e867bc399e9e9c41473408e4df5a7
Nana Yu <nana@kwusen.com>
2025-07-14 19:01:23 -0700

    Remove debug logs

--- src/display/editor/editor.js
+++ src/display/editor/editor.js
@@ -2232,7 +2232,6 @@ class AnnotationEditor {
       this._editToolbar.show();
       return;
     }
-    console.trace("editor select", this.id, this.annotationElementId);
     this.isSelected = true;
     this.makeResizable();
     this.div?.classList.add("selectedEditor");
@@ -2264,7 +2263,6 @@ class AnnotationEditor {
     if (!this.isSelected) {
       return;
     }
-    console.trace("editor unselect", this.id, this.annotationElementId);
     this.isSelected = false;
     this.#resizersDiv?.classList.add("hidden");
     this.div?.classList.remove("selectedEditor");
