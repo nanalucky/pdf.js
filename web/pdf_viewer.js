2202bc6c9d56d2a1eb3740754fc8aa926f0fa93f
Calixte Denizet <calixte.denizet@gmail.com>
2025-07-28 17:42:13 +0200

    Disable printing when enablePermission is true and the pdf isn't allowed to be printed (bug 1978985)

--- web/pdf_viewer.js
+++ web/pdf_viewer.js
@@ -256,6 +256,8 @@ class PDFViewer {
 
   #mlManager = null;
 
+  #printingAllowed = true;
+
   #scrollTimeoutId = null;
 
   #switchAnnotationEditorModeAC = null;
@@ -418,6 +420,10 @@ class PDFViewer {
       // Ensure that Fluent is connected in e.g. the COMPONENTS build.
       this.l10n.translate(this.container);
     }
+  }
+
+  get printingAllowed() {
+    return this.#printingAllowed;
   }
 
   get pagesCount() {
@@ -677,8 +683,22 @@ class PDFViewer {
       textLayerMode: this.#textLayerMode,
     };
     if (!permissions) {
+      this.#printingAllowed = true;
+      this.eventBus.dispatch("printingallowed", {
+        source: this,
+        isAllowed: this.#printingAllowed,
+      });
+
       return params;
     }
+
+    this.#printingAllowed =
+      permissions.includes(PermissionFlag.PRINT_HIGH_QUALITY) ||
+      permissions.includes(PermissionFlag.PRINT);
+    this.eventBus.dispatch("printingallowed", {
+      source: this,
+      isAllowed: this.#printingAllowed,
+    });
 
     if (
       !permissions.includes(PermissionFlag.COPY) &&
@@ -848,6 +868,8 @@ class PDFViewer {
 
       this.#annotationEditorUIManager?.destroy();
       this.#annotationEditorUIManager = null;
+
+      this.#printingAllowed = true;
     }
 
     this.pdfDocument = pdfDocument;
